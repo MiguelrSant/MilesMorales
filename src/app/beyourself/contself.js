cf02dec44d969b7811bdcd89b6614da81615de38
Miguel Angelo <miguelrsant01@gmail.com>
2023-12-26 23:36:39 +0000

    Finalização da pagina beyourself e menu hamburguer melhorado

---
+++ src/app/beyourself/contself.js
@@ -0,0 +1,22 @@
+export default function Self(){
+    return(
+        <section className="cont-self">
+            <div className="imgs-self">
+                <img className="spidermiles" src="/miles-spider.png"/>
+                <img className="miles-text" src="text-miles.png" />
+            </div>
+            <div className="text-self">
+            <p>
+            Ser você mesmo é um desafio para qualquer pessoa, mas especialmente para alguém que tem uma identidade secreta como o Spider-Man. Miles Morales sabe bem disso, pois ele é um adolescente que precisa equilibrar sua vida escolar, familiar e social com sua responsabilidade de proteger Nova York dos vilões.
+                </p>
+                <p>
+                Miles Morales é um jovem de origem afro-latina que herdou os poderes do Spider-Man original, Peter Parker, depois de ser picado por uma aranha radioativa. Ele também tem habilidades únicas, como a capacidade de se tornar invisível e de soltar choques elétricos. Ele é um fã de música, arte e ciência, e tem um grande senso de humor.
+                </p>
+                <p>
+                Miles Morales é um exemplo de como ser você mesmo é uma força, não uma fraqueza. Ele mostra que é possível ser fiel aos seus valores, às suas origens e aos seus sonhos, sem deixar de ser um herói. Ele mostra que ser o Spider-Man não é apenas usar uma máscara, mas sim expressar sua personalidade, sua criatividade e sua coragem.
+                </p>
+                <span><a href="https://www.playstation.com/pt-br/games/marvels-spider-man-miles-morales/" className="btn-secondary " style={{ marginLeft: '20px', marginTop: '20px'}}>Jogar AGORA</a></span>
+            </div>
+        </section>
+    )
+}
--- src/app/beyourself/page.js
+++ src/app/beyourself/page.js
@@ -1,10 +1,14 @@
 import Styles from '../css/index.css'
 import Header from '../PartWebsite/header'
+import Video from '../PartWebsite/video'
+import Self from './contself'
 
 export default function wallpapers() {
   return (
     <div>
       <Header />
+      <Video />
+      <Self />
     </div>
   )
 }
